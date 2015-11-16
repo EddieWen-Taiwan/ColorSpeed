860c1d4a22ccd4adce384277237a99a0daef90ae
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-16 20:58:26 +0800

    Add button action - geameStart()

--- ViewController.swift
+++ ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet var gameViewContainer: UIView!
 
+    @IBOutlet var startView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func gameStart(sender: AnyObject) {
+        self.startView.hidden = true
+        self.gameViewContainer.hidden = false
     }
 
     override func didReceiveMemoryWarning() {

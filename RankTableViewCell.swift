1a3c9fcd97f2dbdb6cc19f1e88e1d6bc67a34b22
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-17 20:49:36 +0800

    Add RankTableViewCell.swift

---
+++ RankTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RankTableViewCell.swift
+//  ColorSpeed
+//
+//  Created by Eddie on 11/17/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class RankTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

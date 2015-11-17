9067f36d3d5562b2a31ac3f3e9ac3a08366eb3a1
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-17 18:52:20 +0800

    Connect 'play again' button to GamingViewController.swift

--- GamingViewController.swift
+++ GamingViewController.swift
@@ -181,6 +181,9 @@ class GamingViewController: UIViewController {
     // *************
     // Ending View
 
+    @IBAction func playAgain(sender: AnyObject) {
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

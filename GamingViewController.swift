8d1de90f8b09d61dbf76f8d6172f3ba6f50ee38a
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-17 16:24:37 +0800

    Add comment

--- GamingViewController.swift
+++ GamingViewController.swift
@@ -157,6 +157,12 @@ class GamingViewController: UIViewController {
         }
     }
 
+
+
+
+    // *************
+    // Ending View
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

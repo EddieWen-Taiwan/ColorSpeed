afd53eb3bb87cd8bd90dcfc42c14e7bcfb5abb8f
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-18 19:34:49 +0800

    Add function to download image

--- Reachability.swift
+++ Reachability.swift
@@ -6,29 +6,36 @@
 //  Copyright © 2015 Wen. All rights reserved.
 //
 
+import Foundation
 import SystemConfiguration
 
-public class Reachability {
+class Reachability {
 
     public func isConnectedToNetwork() -> Bool {
-        
+
         var zeroAddress = sockaddr_in()
         zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
-        
+
         let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
             SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
         }
-        
+
         var flags = SCNetworkReachabilityFlags()
         if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
             return false
         }
-        
+
         let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
         let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-        
+
         return (isReachable && !needsConnection)
+    }
+
+    public func getImageFromUrl( url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void) ) {
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            completion(data: data, response: response, error: error)
+        }.resume()
     }
 
 }

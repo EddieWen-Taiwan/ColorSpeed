26af47e8dc1c5913663214b4d844fc35f7fd2e8e
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-19 09:46:42 +0800

    Connect 'SEND' button to action in GamingViewController.swift

--- GamingViewController.swift
+++ GamingViewController.swift
@@ -273,6 +273,9 @@ class GamingViewController: UIViewController {
 
     }
 
+    @IBAction func sendUpdateRequest(sender: AnyObject) {
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

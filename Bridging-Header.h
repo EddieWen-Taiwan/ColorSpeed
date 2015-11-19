968034713133b70e60ce73f8dbfa97151841922c
EddieWen-Taiwan <m20021991y@gmail.com>
2015-11-19 15:56:16 +0800

    Add Bridging-Header.h

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  ColorSpeed
+//
+//  Created by Eddie on 11/19/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */

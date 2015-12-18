1d00eba701f376ad5d3ba2b06859a257fa7dbdb1
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-18 11:42:03 +0800

    Set containerView content programmingly

--- CenterViewController.swift
+++ CenterViewController.swift
@@ -10,4 +10,51 @@ import UIKit
 
 class CenterViewController: UIViewController {
 
+    @IBOutlet var containerView: UIView!
+
+    private var currentViewController: UIViewController? {
+        didSet {
+            removeInactiveViewController(oldValue)
+            updateActiveViewController()
+        }
+    }
+
+    var startViewController: StartViewController?
+
+
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        currentViewController = startViewController
+    }
+
+    // Copy
+    private func removeInactiveViewController(inactiveViewController: UIViewController?) {
+        if let inActiveVC = inactiveViewController {
+            // call before removing child view controller's view from hierarchy
+            inActiveVC.willMoveToParentViewController(nil)
+            
+            inActiveVC.view.removeFromSuperview()
+            
+            // call after removing child view controller's view from hierarchy
+            inActiveVC.removeFromParentViewController()
+        }
+    }
+    
+    private func updateActiveViewController() {
+        if let activeVC = currentViewController {
+            // call before adding child view controller's view as subview
+            addChildViewController(activeVC)
+            
+            activeVC.view.frame = containerView.bounds
+            containerView.addSubview(activeVC.view)
+            
+            // call before adding child view controller's view as subview
+            activeVC.didMoveToParentViewController(self)
+        }
+    }
+
 }

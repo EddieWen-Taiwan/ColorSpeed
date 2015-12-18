b4af1256013dabb653614bc5a1589c6b78b5f123
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-18 11:28:58 +0800

    Add MainViewController to control all container view

---
+++ CenterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.swift
+//  ColorSpeed
+//
+//  Created by Eddie on 12/18/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class CenterViewController: UIViewController {
+
+}

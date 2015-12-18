da7eccdcbfd33d116fa9fdddeb3559ece945d111
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-18 11:38:30 +0800

    Add StartViewController

---
+++ StartViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartViewController.swift
+//  ColorSpeed
+//
+//  Created by Eddie on 12/18/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import Cocoa
+
+class StartViewController: UIViewController {
+
+}

72ef120a09a49189defb663035348de38e2f14d1
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-18 16:32:00 +0800

    Add GameViewController

---
+++ GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  ColorSpeed
+//
+//  Created by Eddie on 12/18/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

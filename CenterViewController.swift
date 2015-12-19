7ce2afadb125f025bbf5e7d5bfb63109a09e8cf1
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-19 22:11:29 +0800

    Delete old code

--- CenterViewController.swift
+++ CenterViewController.swift
@@ -41,23 +41,7 @@ class CenterViewController: UIViewController {
         self.currentSecond = 0
 //        self.answeredQuestion = 0
 //        self.clock.text = "0"
-        
-        // Resort array
-//        colorArray = colorArray.sort({ (c1: String, color2: String) -> Bool in
-//            let a = Int( arc4random_uniform(50) )
-//            let b = Int( arc4random_uniform(50) )
-//            return a > b
-//        })
-        
-//        // Set text on the buttons
-//        self.LeftTopButton.setTitle( self.colorArray[0], forState: .Normal )
-//        self.LeftBottomButton.setTitle( self.colorArray[1], forState: .Normal )
-//        self.RightTopButton.setTitle( self.colorArray[2], forState: .Normal )
-//        self.RightBottomButton.setTitle( self.colorArray[3], forState: .Normal )
-//        
-//        // First question
-//        self.updateQuestion()
-        
+
     }
 
     func addTimer( timer: NSTimer ) {

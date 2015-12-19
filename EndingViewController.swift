834c3d8315570bc1715813d487b8729e27222177
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-19 23:27:40 +0800

    Add EndingViewController.swift

---
+++ EndingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EndingViewController.swift
+//  ColorSpeed
+//
+//  Created by Eddie on 12/19/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class EndingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

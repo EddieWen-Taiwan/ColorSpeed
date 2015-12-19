81a2e57dfea1ac86f183129129c99e24f8dcdc34
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-20 00:09:34 +0800

    Display final time and animation

--- EndingViewController.swift
+++ EndingViewController.swift
@@ -12,8 +12,18 @@ class EndingViewController: UIViewController {
 
     @IBOutlet var finalTimeLabel: UILabel!
 
+    var parentVC: CenterViewController?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.parentVC = self.parentViewController as? CenterViewController
+
+        self.finalTimeLabel.text = parentVC?.currentSecond.displayText()
+        UIView.animateWithDuration( 1, animations: {
+            self.finalTimeLabel.transform = CGAffineTransformMakeScale(1.7, 1.7)
+            self.view.layoutIfNeeded()
+        })
     }
 
     @IBAction func playAgain(sender: AnyObject) {

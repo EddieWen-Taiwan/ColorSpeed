7b428e9e008f48056684f95649d4524e111d8073
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-23 16:49:40 +0800

    Add BreakViewController and connect to UIViewController

---
+++ BreakViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BreakViewController.swift
+//  ColorSpeed
+//
+//  Created by Eddie on 12/23/15.
+//  Copyright © 2015 Wen. All rights reserved.
+//
+
+import UIKit
+
+class BreakViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

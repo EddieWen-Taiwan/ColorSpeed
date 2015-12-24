76bd0bebbcddb0188e916dbdde7031f24ff7b67b
EddieWen-Taiwan <m20021991y@gmail.com>
2015-12-24 22:03:36 +0800

    Remove useless function

--- RankTableViewController.swift
+++ RankTableViewController.swift
@@ -209,9 +209,4 @@ class RankTableViewController: UITableViewController {
     }
     */
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
